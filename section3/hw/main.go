aa585c1927572b436b5a648d0883dfd6530ff0ff
Simon Wardle <simon.wardle@outlook.com>
2023-04-11 16:28:40 +0100

    Added divide page and error chack

--- section3/hw/main.go
+++ section3/hw/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -21,9 +22,32 @@ func addValues(x, y int) int {
 	return x + y
 }
 
+// Divide is the divide page handler
+func Divide(w http.ResponseWriter, r *http.Request){
+	var x, y float32
+	x = 100.0
+	y = 0.0
+	sum, err := divideValues(x, y)
+	if err != nil {
+		_, _ = fmt.Fprintf(w, fmt.Sprintf("%s", err))
+		return
+	}
+	
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", x, y, sum))
+}
+
+func divideValues(x, y float32) (float32, error) {
+	if y == 0 {
+		err := errors.New("Cannot divide by zero!")
+		return 0, err
+	}
+	return x / y, nil
+}
+
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
